0d6b20f78acdf67b398a6101df7fb16dfe6fc2ce
Max Rebryk <rebryk2003@gmail.com>
2024-06-08 10:24:04 +0300

    1

---
+++ redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const rootReducer = (state, action) => {
+    state
+}
+
+export const store = configureStore({
+    reducer: rootReducer,
+    state:,
+});
